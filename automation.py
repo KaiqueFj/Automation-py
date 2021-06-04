ca8a5335138878e67789fe0348b188fc7b7acf61
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-04 13:02:50 -0300

    adding features

--- automation.py
+++ automation.py
@@ -6,5 +6,21 @@ chrome_browser.maximize_window()
 chrome_browser.get( 'https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
 assert('Selenium Easy Demo' in chrome_browser.title)
-button_text = chrome_browser.find_element_by_class_name('btn-default')
-print(button_text.get_attribute('innerHTML'))
+
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+
+
+assert('Show Message' in chrome_browser.page_source)
+
+user_massage = chrome_browser.find_element_by_id('user-message')
+user_massage.clear()
+user_massage.send_keys('I am Python dev')
+
+show_message_button.click()
+output_message = chrome_browser.find_element_by_id('display')
+
+assert('I am Python dev' in output_message.text)
+
+print(user_button2)
+print(show_message_button)
